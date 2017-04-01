d7d23b36247e7c80e3a23ae7c9b31f381c67158e
quyencao <boy20195@gmail.com>
2017-04-01 21:30:55 +0700

    Make search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyD-V1jxPP1NsEgvV7Lcr-d5sfL7SwnHpHE';
 
 const App = () => {
   return (
-    <div>Hi!</div>
+    <div>
+      <SearchBar />
+    </div>
   )
 }
 
