5c2c80a590960f17526b886ec388c522efda1494
quyencao <boy20195@gmail.com>
2017-04-01 23:42:40 +0700

    Change App to class components and use state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,16 +6,26 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD-V1jxPP1NsEgvV7Lcr-d5sfL7SwnHpHE';
 
-YTSearch({ key: API_KEY, term: 'football' }, function(data) {
-    console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'football' }, (videos) => {
+        this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
