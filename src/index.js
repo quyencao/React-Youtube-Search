05c37a84e4d6209928641b37b9373714bcf27e95
quyencao <boy20195@gmail.com>
2017-04-01 23:26:24 +0700

    User youtube api search package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD-V1jxPP1NsEgvV7Lcr-d5sfL7SwnHpHE';
+
+YTSearch({ key: API_KEY, term: 'football' }, function(data) {
+    console.log(data);
+});
 
 const App = () => {
   return (
