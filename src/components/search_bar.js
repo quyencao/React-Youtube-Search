369308e2bc1d7e0bc76a51f0e49a6ca9c0c703a6
quyencao <boy20195@gmail.com>
2017-04-01 22:13:47 +0700

    State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  construtor(props) {
+      super(props);
+
+      this.state = { term: '' };
+  }
+
   render() {
     return <input onChange={event => console.log(event.target.value)}/>;
   }
-
-  // this.onInputChange
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
 }
 
 export default SearchBar;
